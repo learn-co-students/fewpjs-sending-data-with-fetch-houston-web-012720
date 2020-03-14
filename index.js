d372336cb673a35e94d43cea3525ca7b9f16cf1a
somaia-khalil <sumaya_khair@live.com>
2020-03-14 16:05:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    let config = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "accept": "application/json"
+
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", config)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            let body = document.querySelector("body")
+            let p = document.createElement("p")
+            p.innerHTML = object.id
+            body.append(p)
+        })
+        .catch(function(error) {
+            let p = document.createElement('p')
+            p.innerHTML = error.message
+            document.body.append(p)
+        });
+
+}
